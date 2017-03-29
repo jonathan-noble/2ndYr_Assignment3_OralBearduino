0f49f7adb43b71a335adf8283e66cbb4aad67c38
Butan <c15487922@mydit.ie>
2017-03-29 13:47:28 +0100

    Restarted IDE to Android Studio

--- OralBear/app/src/main/java/ie/dit/MainActivity.java
+++ OralBear/app/src/main/java/ie/dit/MainActivity.java
@@ -1,16 +1,56 @@
 package ie.dit;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+//import android.media.AudioManager;
+//import android.media.MediaPlayer;
+//import android.os.Bundle;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.button;
 
-public class MainActivity extends AppCompatActivity {
 
-    String menu[]={"Main Activity", "AudioPlayer"};
+public class MainActivity {// extends AppCombatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+
+
+   // MediaPlayer mySound;
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        mySound = MediaPlayer.create(this, R.raw.shooting_stars);
+//
+//         Button play = (Button) findViewbyId(R.id.button_play);
+////         Button pause = (Button) findViewbyId(R.id.button_pause);
+////         Button stop = (Button) findViewbyId(R.id.button_stop);
+//    }
+//
+//    public void playMusic(View view) {
+//    mySound.start();
+//    }
+//
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        //Inflate the menu; this adds items to the action bar if itis present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 }
+
