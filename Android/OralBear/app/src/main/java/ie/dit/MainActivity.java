d4d05f4c0317b7448b42ffc8043fa755f6c6e729
Butan <c15487922@mydit.ie>
2017-03-31 23:58:55 +0100

    App Update

--- Android/OralBear/app/src/main/java/ie/dit/MainActivity.java
+++ Android/OralBear/app/src/main/java/ie/dit/MainActivity.java
@@ -1,33 +1,29 @@
 package ie.dit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 //import android.media.AudioManager;
 //import android.media.MediaPlayer;
-//import android.os.Bundle;
 //import android.view.Menu;
 //import android.view.MenuItem;
 //import android.view.View;
 //import android.button;
 
 
-public class MainActivity {// extends AppCombatActivity {
-
-
-
+public class MainActivity extends AppCompatActivity {
 
    // MediaPlayer mySound;
 
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 //        mySound = MediaPlayer.create(this, R.raw.shooting_stars);
 //
 //         Button play = (Button) findViewbyId(R.id.button_play);
 ////         Button pause = (Button) findViewbyId(R.id.button_pause);
 ////         Button stop = (Button) findViewbyId(R.id.button_stop);
-//    }
+      }
 //
 //    public void playMusic(View view) {
 //    mySound.start();
