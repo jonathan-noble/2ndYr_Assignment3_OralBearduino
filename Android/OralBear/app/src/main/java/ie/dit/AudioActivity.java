02a7059faba69b67ee4fc7cb8a8c22609ea78681
Butan <c15487922@mydit.ie>
2017-04-04 18:19:53 +0100

    Update on Buttons

--- Android/OralBear/app/src/main/java/ie/dit/AudioActivity.java
+++ Android/OralBear/app/src/main/java/ie/dit/AudioActivity.java
@@ -9,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.media.MediaPlayer;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 
 
 
--- Android/OralBear/app/src/main/java/ie/dit/MainActivity.java
+++ Android/OralBear/app/src/main/java/ie/dit/MainActivity.java
@@ -8,11 +8,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
-//import android.view.Menu;
-//import android.view.MenuItem;
+import android.view.Menu;
+import android.view.MenuItem;
 //import android.media.AudioManager;
 //import android.media.MediaPlayer;
-//import android.button;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,10 +26,27 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onAudio(View v) {
+    public void ListenAudio(View v) {
 
         startActivity(new Intent(this, AudioActivity.class));
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 
 
@@ -43,25 +60,5 @@ public class MainActivity extends AppCompatActivity {
 //    public void playMusic(View view) {
 //    mySound.start();
 //    }
-//
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        //Inflate the menu; this adds items to the action bar if itis present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
 }
 
