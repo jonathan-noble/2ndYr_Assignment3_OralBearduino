4805899c135c622864a9571ecb6c1f39ac68bfe3
PiaOfalsa <c15734155@mydit.ie>
2017-04-04 20:24:43 +0100

    Skeleton code of LCD Screen

---
+++ oralBear_gui/oralBear_gui.ino
@@ -0,0 +1,22 @@
+/*
+    User interface for Robobearduino, 
+    will be displayed on the tft lcd screen
+    1.home menu
+      options:
+        -timer
+        -game
+        -tips/advice on how to brush teeth
+*/
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
