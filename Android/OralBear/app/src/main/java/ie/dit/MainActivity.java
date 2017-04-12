74a7967b715c5bcc607412c1756c6053bd2545b9
PiaOfalsa <c15734155@mydit.ie>
2017-04-12 22:37:18 +0100

    frag functionality added in main

--- Android/OralBear/app/src/main/java/ie/dit/MainActivity.java
+++ Android/OralBear/app/src/main/java/ie/dit/MainActivity.java
@@ -41,6 +41,44 @@ public class MainActivity extends AppCompatActivity {
         //calling layout
         setContentView(R.layout.activity_main);
 
+        //fragment code
+
+        // Allows you to interact with Fragments in an Activity
+        FragmentManager fragmentManager = getFragmentManager();
+
+        // beginTransaction() begins the FragmentTransaction which allows you to
+        // add, attach, detach, hide, remove, replace, animate, transition or
+        // show fragments
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+
+        // The Configuration object provides device configuration info
+        // http://developer.android.com/reference/android/content/res/Configuration.html
+        Configuration configInfo = getResources().getConfiguration();
+
+        // Depending on the screen orientation replace with the correct fragment
+        if(configInfo.orientation == Configuration.ORIENTATION_LANDSCAPE){
+
+            FragmentLandscape fragmentLandscape = new FragmentLandscape();
+
+            fragmentTransaction.replace(android.R.id.content,
+                    fragmentLandscape);
+
+        } else {
+
+            FragmentPortrait fragmentPortrait = new FragmentPortrait();
+
+            fragmentTransaction.replace(android.R.id.content,
+                    fragmentPortrait);
+
+        }
+
+        // Schedule for the replacement of the Fragment as soon as possible
+        fragmentTransaction.commit();
+
+
+
+        //end of fragment code
         // sharedPref= PreferenceManager.getDefaultSharedPreferences(this);
 
         //initialised buttons
